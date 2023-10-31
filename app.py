f7bee735cf50c1a3942e6e7a2c3a67886f412f33
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-10-31 15:09:37 +0100

    update tutorial

--- app.py
+++ app.py
@@ -32,9 +32,15 @@ You can additionally save :
 - Your character levels
 - Your klix
 
-If you fill the "Cookie" field. Please contact us on the [Eternaltwin Discord](https://discord.gg/Kd8DUkWy4N) or Evian on Twinoid to get help with this.
+If you fill the "Cookie" field. For that you wll find below two links to copy your cookies : one for the French server and one for the English server.
 
-You can only backup the profile from **one server**. Choose wisely!
+Save the link that interests you as a new bookmark (right-click and save the bookmark). 
+
+Next, go to the corresponding Mush site and log in to your account. Click on the bookmark saved previously while being logged into the Mush site.
+
+This will automatically save the cookie in your clipboard. Return to the eMush import page, and paste the cookie into the 'cookie' field below.
+
+You can only backup the profile from **one server**. Choose wisely! 
 
 Do not leave the page during the profile import!
 
@@ -155,6 +161,7 @@ if __name__ == "__main__":
     
     if "code" in st.experimental_get_query_params():
         st.info(TUTO)
+        st.components.v1.html(cookies, height=40)
         code = st.experimental_get_query_params()["code"][0]
         profile_language_to_save = st.selectbox("Profile to save", ["French", "English"])
         sid = st.text_input("Cookie", "")
