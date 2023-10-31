f021952f5ea60af9a633aa32e4c7d9330c1b8322
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-10-31 13:41:08 +0100

    fix: Connect to Twinoid link now uses env var to adapt with environements

--- links.py
+++ links.py
@@ -1,1 +1,3 @@
-connect_to_twinoid = "[Connect to Twinoid](" + "https://twinoid.com/oauth/auth?response_type=code&client_id=407&redirect_uri=http://localhost/&scope=mush.twinoid.com+mush.twinoid.es+mush_ship_data+mush.vg+groups&state=auth&access_type=offline" + ")"
+from streamlit import secrets
+
+connect_to_twinoid = "[Connect to Twinoid](" + f"https://twinoid.com/oauth/auth?response_type=code&client_id={secrets.TWINOID_CLIENT_ID}&redirect_uri={secrets.TWINOID_REDIRECT_URI}&scope=mush.twinoid.com+mush.twinoid.es+mush_ship_data+mush.vg+groups&state=auth&access_type=offline" + ")"
